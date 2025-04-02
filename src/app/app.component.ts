76366d06ee01547d5b1437f1bc5eca09d8a225ea
spranchal <spranchal@indra.es>
2025-04-02 09:09:56 +0200

    created directory structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Button],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client';
+  isDarkMode = false;
+
+  toggleDarkMode() {
+    const element: HTMLElement|null = document.querySelector('html');
+    if (element) element.classList.toggle('my-app-dark');
+  }
 }
