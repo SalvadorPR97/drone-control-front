5353fc15542f5b365f735da0f6ea7c10461d6530
spranchal <spranchal@indra.es>
2025-04-11 08:20:06 +0200

    added some style for flying panel

--- src/app/pages/flying/flying.component.ts
+++ src/app/pages/flying/flying.component.ts
@@ -12,6 +12,7 @@ import {DroneMove} from '../drones/interfaces/DroneMove.interface';
 import {FlyingService} from '../../core/services/flying.service';
 import {ToastModule} from 'primeng/toast';
 import {firstValueFrom} from 'rxjs';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-pages-flying',
@@ -21,7 +22,8 @@ import {firstValueFrom} from 'rxjs';
     SearchButtonComponent,
     DronesTableComponent,
     DroneCommandsComponent,
-    ToastModule
+    ToastModule,
+    NgStyle
   ],
   standalone: true,
   templateUrl: './flying.component.html',
@@ -60,62 +62,39 @@ export class FlyingComponent {
     });
   }
 
-  /*public moveDrone(command: Command): void {
-    if (this.selectedDrones) {
-      this.selectedDrones.forEach((drone: Drone) => {
-        const droneMove: DroneMove = {id : drone.id, matrizId: this.matrix.id, orden: [command]}
-        this.flyingService.moveDrone(droneMove).subscribe({
-          next: (res: Matrix) => {
-            this.matrix = res;
-            this.drones = res.drones;
-            this.messageService.add({
-              severity: 'success',
-              summary: 'Drone moved',
-              detail: `Drone with id ${drone.id} moved successfully`,
-            });
-          },
-          error: (error) => {
-            this.messageService.add({
-              severity: 'error',
-              summary: 'Error creating drone',
-              detail: error.message
-            });
-          }
-        });
-      });
-    }
-  }*/
-
   public async moveDrone(command: Command): Promise<void> {
-    if (this.selectedDrones?.length) {
-      for (const drone of this.selectedDrones) {
-        const droneMove: DroneMove = {
-          id: drone.id,
-          matrizId: this.matrix.id,
-          orden: [command],
-        };
+    if (!this.selectedDrones?.length) return;
 
-        try {
-          const updatedMatrix = await firstValueFrom(this.flyingService.moveDrone(droneMove));
+    let allSuccess: boolean = true;
+    let errormsg: string = "";
 
-          if (updatedMatrix) {
-            this.matrix = updatedMatrix;
-            this.drones = updatedMatrix.drones;
-          }
-          this.messageService.add({
-            severity: 'success',
-            summary: 'Drone moved',
-            detail: `Drone with id ${drone.id} moved successfully`,
-          });
-        } catch (error: any) {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Error moving drone',
-            detail: error.message,
-          });
+    for (const drone of this.selectedDrones) {
+      const droneMove: DroneMove = {
+        id: drone.id,
+        matrizId: this.matrix.id,
+        orden: [command],
+      };
+
+      try {
+        const updatedMatrix = await firstValueFrom(this.flyingService.moveDrone(droneMove));
+        if (updatedMatrix) {
+          this.matrix = updatedMatrix;
+          this.drones = updatedMatrix.drones;
         }
+      } catch (error: any) {
+        allSuccess = false;
+        errormsg = error;
       }
     }
+
+    this.messageService.add({
+      severity: allSuccess ? 'success' : 'error',
+      summary: allSuccess ? 'All drones moved' : 'Error moving some drones',
+      detail: allSuccess
+        ? 'All selected drones moved successfully'
+        : errormsg,
+    });
   }
+
 }
 
